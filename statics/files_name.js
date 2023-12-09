60b3eab226d64c9f48c95817d1e62afc05afe79c
acclimate-sketches <153184263+acclimate-sketches@users.noreply.github.com>
2023-12-09 20:21:49 +0500

    Update File Name

---
+++ statics/files_name.js
@@ -0,0 +1,12 @@
+files_names = [
+'pr_1.jpg',
+'pr_2.jpg',
+'pr_3.jpeg',
+'pr_4.jpg',
+'pr_5.jpeg',
+'pr_6.jpg',
+'pr_7.jpg',
+'pr_8.jpg',
+'pr_9.jpeg',
+'pr_10.jpeg',
+]
